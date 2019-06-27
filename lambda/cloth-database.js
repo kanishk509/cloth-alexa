cf67bdeeb533b64a2fac09814cccdd5d51624511
Kanishk <kanishk509@gmail.com>
2019-06-27 14:28:43 +0530

    Update cloth-database.js
    change DB to use arrays.

--- lambda/cloth-database.js
+++ lambda/cloth-database.js
@@ -1,49 +1,59 @@
-let factorIndex = {
+let factorIndex = [
 	//fixed factors, i.e. physical charecteristics
-	build : {
-		slim      : 0,
-		medium    : 1,
-		heavy     : 2
-	},
-
-	complexion : {
+	//complexion : 0
+	{
+		name 	  : 'complexion',
 		fair      : 0,
 		tan       : 1,
 		dark      : 2
 	},
+	//build : 1
+	{
+		name 	  : 'build',
+		slim      : 0,
+		medium    : 1,
+		heavy     : 2
+	},
 	//height is range based, so a separate function will be used
 
 	//input factors
-	timeOfDay : {
+	//timeOfDay : 2
+	{
+		name 	  : 'timeOfDay',
 		morning   : 0,
 		afternoon : 1,
 		night     : 2
 	},
-
-	occassion : {
+	//occassion : 3
+	{
+		name 	  : 'occassion',
 		office    : 0,
 		outdoor   : 1,
 		casual    : 2,
 		party     : 3
 	},
-};
+];
 
-let clothDB = {
+let dressDB = [
 	//scores out of 100
-	xyzSkirt : {
-		build : [40, 20, 50],				// scores for [slim, medium, heavy]
-		complexion : [10, 30, 20],			// scores for [fair, tan, dark]
-		timeOfDay : [01, 25, 10],			// scores for [morning, afternoon, night]
+	//xyzSkirt : 0
+	{
+		name : 'xyzSkirt',
+		build : [40, 20, 50],			// scores for [slim, medium, heavy]
+		complexion : [10, 30, 20],		// scores for [fair, tan, dark]
+		timeOfDay : [01, 25, 10],		// scores for [morning, afternoon, night]
 		occassion : [100, 20, 03, 15]		// scores for [office, outdoor, casual, party]
 	},
 
-	anotherDress : {
+	//anotherDress : 1
+	{
+		name : 'anotherDress',
 		build : [],
 		complexion : [],
 		timeOfDay : [],
 		occassion : []
 	}
-};
+];
 
 module.exports.factorIndex = factorIndex;
-module.exports.clothDB = clothDB;
+module.exports.dressDB = dressDB;
